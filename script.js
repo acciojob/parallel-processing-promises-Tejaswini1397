451fbabfde6bd863bee68fabb2dbb16f7cc5373a
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2024-01-21 22:00:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,31 +8,52 @@ const images = [
   { url: "https://picsum.photos/id/239/200/300" },
 ];
 
-btn.addEventListener("click", () => {
-      downloadImages(images)
-        .then((downloadedImages) => {
-          output.innerHTML = ""; // Clear previous images
-          downloadedImages.forEach((image) => {
-            const imgElement = document.createElement("img");
-            imgElement.src = image.url;
-            output.appendChild(imgElement);
-          });
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    });
+// btn.addEventListener("click", () => {
+//       downloadImages(images)
+//         .then((downloadedImages) => {
+//           output.innerHTML = ""; // Clear previous images
+//           downloadedImages.forEach((image) => {
+//             const imgElement = document.createElement("img");
+//             imgElement.src = image.url;
+//             output.appendChild(imgElement);
+//           });
+//         })
+//         .catch((error) => {
+//           console.error(error);
+//         });
+//     });
 
-    function loadImage(image) {
-      return new Promise((resolve, reject) => {
-        const img = new Image();
-        img.onload = () => resolve({ url: image.url });
-        img.onerror = () => reject(new Error(`Failed to load image's URL: ${image.url}`));
-        img.src = image.url;
-      });
-    }
+//     function loadImage(image) {
+//       return new Promise((resolve, reject) => {
+//         const img = new Image();
+//         img.onload = () => resolve({ url: image.url });
+//         img.onerror = () => reject(new Error(`Failed to load image's URL: ${image.url}`));
+//         img.src = image.url;
+//       });
+//     }
 
-    function downloadImages(images) {
-      const promises = images.map(loadImage);
-      return Promise.all(promises);
-    }
+//     function downloadImages(images) {
+//       const promises = images.map(loadImage);
+//       return Promise.all(promises);
+//     }
+const imgLoad=(url)=>{
+	return `<img src=${url}/>`;
+}
+const all= async () =>{
+	try{
+		const gallary=await Promise.all(images.map((val)=> fetch(val.url)));
+		gallary.forEach((val)=>{
+			output.insertAdjacentHTML("beforeend",imgLoad(val.url));
+			
+		})
+		return gallary;
+	}catch(error){
+		 return "failed to load images";
+	}
+	
+}
+btn.addEventListener('click',()=>{
+	images.forEach((val)=>{
+		output.insertAdjacentHTML("beforeend",imgLoad(val.url));
+	})
+});
