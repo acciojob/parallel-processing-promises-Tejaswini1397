1b42325cb28e1420ca7fdbf2d1e7889ad4f9d235
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2024-01-21 21:44:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,32 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+btn.addEventListener("click", () => {
+      downloadImages(images)
+        .then((downloadedImages) => {
+          output.innerHTML = ""; // Clear previous images
+          downloadedImages.forEach((image) => {
+            const imgElement = document.createElement("img");
+            imgElement.src = image.url;
+            output.appendChild(imgElement);
+          });
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    });
+
+    function loadImage(image) {
+      return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.onload = () => resolve({ url: image.url });
+        img.onerror = () => reject(new Error(`Failed to load image's URL: ${image.url}`));
+        img.src = image.url;
+      });
+    }
+
+    function downloadImages(images) {
+      const promises = images.map(loadImage);
+      return Promise.all(promises);
+    }
